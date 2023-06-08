81189c227b322c8f99566825074706eaad1623c5
Tokiwokoe <mam4ur.anastasia@gmail.com>
2023-06-09 00:40:36 +0300

    change lookups

--- PharmacyDatabase_root/Pharmacy/urls.py
+++ PharmacyDatabase_root/Pharmacy/urls.py
@@ -3,6 +3,7 @@ from Pharmacy.views import *
 
 urlpatterns = [
     path('', index, name='pharmacy_index'),
+
     path('property_type/', property_type, name='property_type'),
     path('dosage_form/', dosage_form, name='dosage_form'),
     path('district/', district, name='district'),
@@ -11,4 +12,13 @@ urlpatterns = [
     path('pharmacy/', pharmacy, name='pharmacy'),
     path('drug/', drug, name='drug'),
     path('company/', company, name='company'),
+
+    path('property_type/<int:id>/', property_type_change, name='property_type_change'),
+    path('dosage_form/<int:id>/', dosage_form_change, name='dosage_form_change'),
+    path('district/<int:id>/', district_change, name='district_change'),
+    path('country/<int:id>/', country_change, name='country_change'),
+    path('pharmacological_group/<int:id>/', pharmacological_group_change, name='pharmacological_group_change'),
+    path('pharmacy/<int:id>/', pharmacy_change, name='pharmacy_change'),
+    path('drug/<int:id>/', drug_change, name='drug_change'),
+    path('company/<int:id>/', company_change, name='company_change'),
 ]
--- PharmacyDatabase_root/Pharmacy/views.py
+++ PharmacyDatabase_root/Pharmacy/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import *
 
@@ -8,39 +9,99 @@ def index(request):
 
 def property_type(request):
     property_type = PropertyType.objects.all()
-    return render(request, 'pharmacy/lookup.html', {'lookup': property_type, 'title': 'Типы собственности'})
+    return render(request, 'pharmacy/lookup.html', {'name': 'property_type', 'lookup': property_type, 'title': 'Типы собственности'})
 
 
 def dosage_form(request):
     dosage_form = DosageForm.objects.all()
-    return render(request, 'pharmacy/lookup.html', {'lookup': dosage_form, 'title': 'Формы выпуска'})
+    return render(request, 'pharmacy/lookup.html', {'name': 'dosage_form', 'lookup': dosage_form, 'title': 'Формы выпуска'})
 
 
 def district(request):
     district = District.objects.all()
-    return render(request, 'pharmacy/lookup.html', {'lookup': district, 'title': 'Районы'})
+    return render(request, 'pharmacy/lookup.html', {'name': 'district', 'lookup': district, 'title': 'Районы'})
 
 
 def country(request):
     country = Country.objects.all()
-    return render(request, 'pharmacy/lookup.html', {'lookup': country, 'title': 'Страны'})
+    return render(request, 'pharmacy/lookup.html', {'name': 'country', 'lookup': country, 'title': 'Страны'})
 
 
 def pharmacological_group(request):
     pharmacological_group = PharmacologicalGroup.objects.all()
-    return render(request, 'pharmacy/lookup.html', {'lookup': pharmacological_group, 'title': 'Фармакологические группы'})
+    return render(request, 'pharmacy/lookup.html', {'name': 'pharmacological_group', 'lookup': pharmacological_group, 'title': 'Фармакологические группы'})
 
 
 def pharmacy(request):
     pharmacy = Pharmacy.objects.all()
-    return render(request, 'pharmacy/pharmacy.html', {'table': pharmacy, 'title': 'Аптеки'})
+    return render(request, 'pharmacy/pharmacy.html', {'name': 'pharmacy', 'table': pharmacy, 'title': 'Аптеки'})
 
 
 def drug(request):
     drug = Drug.objects.all()
-    return render(request, 'pharmacy/drug.html', {'table': drug, 'title': 'Препараты'})
+    return render(request, 'pharmacy/drug.html', {'name': 'drug', 'table': drug, 'title': 'Препараты'})
 
 
 def company(request):
     company = Company.objects.all()
-    return render(request, 'pharmacy/company.html', {'table': company, 'title': 'Компании-производители'})
+    return render(request, 'pharmacy/company.html', {'name': 'company', 'table': company, 'title': 'Компании-производители'})
+
+
+def property_type_change(request, id):
+    lookup = PropertyType.objects.get(id=id)
+    if request.method == 'POST':
+        lookup.name = request.POST.get('name')
+        lookup.save()
+        return redirect('property_type')  # Перенаправление на страницу со списком районов
+    return render(request, 'pharmacy/lookup_change.html', {'lookup': lookup, 'name': 'property_type'})
+
+
+def dosage_form_change(request, id):
+    lookup = DosageForm.objects.get(id=id)
+    if request.method == 'POST':
+        lookup.name = request.POST.get('name')
+        lookup.save()
+        return redirect('dosage_form')  # Перенаправление на страницу со списком районов
+    return render(request, 'pharmacy/lookup_change.html', {'lookup': lookup, 'name': 'dosage_form'})
+
+
+def district_change(request, id):
+    lookup = District.objects.get(id=id)
+    if request.method == 'POST':
+        lookup.name = request.POST.get('name')
+        lookup.save()
+        return redirect('district')  # Перенаправление на страницу со списком районов
+    return render(request, 'pharmacy/lookup_change.html', {'lookup': lookup, 'name': 'district'})
+
+
+def country_change(request, id):
+    lookup = Country.objects.get(id=id)
+    if request.method == 'POST':
+        lookup.name = request.POST.get('name')
+        lookup.save()
+        return redirect('country')  # Перенаправление на страницу со списком районов
+    return render(request, 'pharmacy/lookup_change.html', {'lookup': lookup, 'name': 'country'})
+
+
+def pharmacological_group_change(request, id):
+    lookup = PharmacologicalGroup.objects.get(id=id)
+    if request.method == 'POST':
+        lookup.name = request.POST.get('name')
+        lookup.save()
+        return redirect('pharmacological_group')  # Перенаправление на страницу со списком районов
+    return render(request, 'pharmacy/lookup_change.html', {'lookup': lookup, 'name': 'pharmacological_group'})
+
+
+def pharmacy_change(request, id):
+    table = Pharmacy.objects.get(id=id)
+    return render(request, 'pharmacy/pharmacy_change.html', {'table': table})
+
+
+def drug_change(request, id):
+    table = Drug.objects.get(id=id)
+    return render(request, 'pharmacy/drug_change.html', {'table': table})
+
+
+def company_change(request, id):
+    table = Company.objects.get(id=id)
+    return render(request, 'pharmacy/company_change.html', {'table': table})
